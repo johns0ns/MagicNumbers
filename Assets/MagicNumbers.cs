a4d6c58470552b9157e5ac234e35a4029d71cd81
Artem <krekotnev@gmail.com>
2020-01-30 21:13:20 +0300

    Complete Game Flow

--- Assets/MagicNumbers.cs
+++ Assets/MagicNumbers.cs
@@ -9,38 +9,57 @@ public class MagicNumbers : MonoBehaviour
     int maxNumber = 1000;
     string playerName = "Artemka";
     int guess;
-    int guess1;
+    
 
     //  bool isGameStarted = false;
 
     void Start()
     {
         Debug.Log("Привет :) " + playerName);
+        StartGame();
+
+        
+
+    }
+    void StartGame()
+    {
+        minNumber = 1;
+        maxNumber = 1000;
+
         Debug.Log("Загадайте число");
         Debug.Log("Минимальноe число: " + minNumber);
         Debug.Log("Максимальноe число: " + maxNumber);
+        UpdateGuess();
+    }
 
+    void UpdateGuess()
+    {
         guess = (minNumber + maxNumber) / 2;
         Debug.Log("А число твоё: " + guess + " ?");
-        
     }
 
     void Update()
     {
         
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            Debug.Log("Мой IQ не меньше Стивена Хокинга хи-хи-хи :)! Твоё число было : "+ guess);
-        }
+       
 
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Сча подумаем, твоё число:" + guess);
+            
+            minNumber = guess;
+            UpdateGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Ммммм, твоё число в таком случае:" +);
+            maxNumber = guess;
+            UpdateGuess();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Debug.Log("Сча подумаем, твоё число:" + guess);
+            StartGame();
         }
 
 
