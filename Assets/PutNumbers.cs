3738a4d117e4a2393f8453a0e8d91feeff6c5b44
johns0ns <krekotnev@gmail.com>
2020-02-03 20:58:41 +0300

    Homework 1

--- Assets/PutNumbers.cs
+++ Assets/PutNumbers.cs
@@ -5,16 +5,116 @@ using UnityEngine;
 public class PutNumbers : MonoBehaviour
 {
     int num;
-    
+    int n;
+    string player = "юзер";
+    int click;
+    int tap;
+
     void Start()
     {
-        Debug.Log("Нажмите цифру от 1 до 9 ");
-
+        Debug.Log("Я хочу сыграть в игру... Нажми любую цифру: ");
+        StartGame();
     }
 
-    // Update is called once per frame
+    void StartGame()
+    {
+        click = 0;
+        num = 0;
+        n = 0;
+        UpdateNum();
+    }
+
+    void UpdateNum()
+    {
+        num = num + n;
+        click = click + tap;
+        if (num != 0)
+        {
+            Debug.Log("Сумма : " + num);
+        }
+    }
+
     void Update()
     {
-        
+       if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            tap = 1;
+            n = 1;
+            UpdateNum();
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            tap = 1;
+            n = 2;
+            UpdateNum();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            tap = 1;
+            n = 3;
+            UpdateNum();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            tap = 1;
+            n = 4;
+            UpdateNum();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            tap = 1;
+            n = 5;
+            UpdateNum();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            tap = 1;
+            n = 6;
+            UpdateNum();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            tap = 1;
+            n = 7;
+            UpdateNum();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            tap = 1;
+            n = 8;
+            UpdateNum();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            tap = 1;
+            n = 9;
+            UpdateNum();
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            num = 0;
+            n = 0;
+            Debug.Log("Вот нафига ты нажал пробел :facepalm: .... Лаааадно, начинаем сначала )");
+            UpdateNum();
+
+        }
+
+        else if (num>=50)
+        {
+            Debug.Log("Happy end :D !!!  Сумма получилась: " + num + " Количество ходов:" + click);
+            Start();
+        }
+
     }
 }
