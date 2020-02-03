40b24568504587f94d887154ce1624eb954071fd
johns0ns <krekotnev@gmail.com>
2020-02-03 20:23:02 +0300

    Homework MagicNumbers
    who's daddy? ))))

--- Assets/MagicNumbers.cs
+++ Assets/MagicNumbers.cs
@@ -7,25 +7,20 @@ public class MagicNumbers : MonoBehaviour
 
     int minNumber = 1;
     int maxNumber = 1000;
-    string playerName = "Artemka";
+    string playerName = "Артём";
     int guess;
-    
-
-    //  bool isGameStarted = false;
-
+    int click;
+    int tap;
     void Start()
     {
         Debug.Log("Привет :) " + playerName);
         StartGame();
-
-        
-
     }
     void StartGame()
     {
         minNumber = 1;
         maxNumber = 1000;
-
+        click = 0;
         Debug.Log("Загадайте число");
         Debug.Log("Минимальноe число: " + minNumber);
         Debug.Log("Максимальноe число: " + maxNumber);
@@ -36,32 +31,31 @@ public class MagicNumbers : MonoBehaviour
     {
         guess = (minNumber + maxNumber) / 2;
         Debug.Log("А число твоё: " + guess + " ?");
+        click = click + tap;
     }
 
     void Update()
     {
-        
-       
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            
             minNumber = guess;
+            tap = 1;
             UpdateGuess();
+            
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             maxNumber = guess;
+            tap = 1;
             UpdateGuess();
-
+            
         }
         else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
-            Debug.Log("Сча подумаем, твоё число:" + guess);
+            Debug.Log("Сча подумаем, твоё число:" + guess + " и ты накликал " + click + " раз :)");
             StartGame();
         }
-
-
     }
 }
---
+++ Assets/PutNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutNumbers : MonoBehaviour
+{
+    int num;
+    
+    void Start()
+    {
+        Debug.Log("Нажмите цифру от 1 до 9 ");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
