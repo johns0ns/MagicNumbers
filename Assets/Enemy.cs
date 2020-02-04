1c7f74ad17ebad0c3e2418bd084daf8bd9343021
Artem <krekotnev@gmail.com>
2020-02-04 21:13:29 +0300

    Lesson 3

---
+++ Assets/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Player Player123;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player123.playerName = "BAAAD PLAYER";
+        Debug.Log("Hello " + Player123.playerName + " I am your enemy");
+
+    }
+
+    public void RunAway()
+    {
+        Debug.Log("I'm Scared");
+    }
+    void Win()
+    {
+     Debug.Log("I'm Winner");
+
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string playerName;
+    public Enemy enemy2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello,my name is " + playerName); 
+        Debug.Log(enemy2);
+        enemy2.RunAway();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
